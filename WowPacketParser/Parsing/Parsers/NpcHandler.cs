55320b8c3500e852cf2cc865ac31c37fa298678f
funjoker <funjoker109@gmail.com>
2020-09-15 11:01:34 +0200

    Fix GossipParse
    SMSG_GOSSIP_COMPLETE sometimes appears before GossipMessage, so its not a trustful info that a gossip is closed

--- WowPacketParser/Parsing/Parsers/NpcHandler.cs
+++ WowPacketParser/Parsing/Parsers/NpcHandler.cs
@@ -591,7 +591,6 @@ namespace WowPacketParser.Parsing.Parsers
         [Parser(Opcode.SMSG_GOSSIP_COMPLETE)]
         public static void HandleGossipComplete(Packet packet)
         {
-            LastGossipOption.Reset();
         }
 
         [Parser(Opcode.SMSG_THREAT_UPDATE)]
