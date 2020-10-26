TrinityCore/WowPacketParser

Add TBC parsing improvements: (#543)
* Add support for reading non-tiawps packet headers
* Enable parsing of TBC for database
* Add new config enum versions
Evil TBC maniac forces good TC people to do his bidding.
I hate the fact I had to rename the enum and WOTLK is not "1"

* Enable parsing of SpellEffect.CallPet

WowPacketParser/Store/Objects/GameObjectModel.cs
@@ -28,13 +28,13 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("spawnDifficulties", TargetedDatabase.Legion)]
         public string spawnDifficulties;
 
-        [DBFieldName("phaseMask", TargetedDatabase.Zero, TargetedDatabase.Cataclysm)]
+        [DBFieldName("phaseMask", TargetedDatabase.WrathOfTheLichKing, TargetedDatabase.Cataclysm)]
         public uint? PhaseMask;
 
         [DBFieldName("PhaseId", TargetedDatabase.Cataclysm)]
         public string PhaseID;
 
-        [DBFieldName("PhaseGroup")]
+        [DBFieldName("PhaseGroup", TargetedDatabase.Cataclysm)]
         public uint? PhaseGroup;
 
         [DBFieldName("position_x")]

cyberium/ratkosrbWowPacketParser

WowPacketParser/Store/Objects/GameObjectModel.cs
@@ -28,13 +28,13 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("spawn_difficulties", TargetedDatabase.Legion)]
         public string spawnDifficulties;
 
-        [DBFieldName("phase_mask", TargetedDatabase.Zero, TargetedDatabase.Cataclysm)]
+        [DBFieldName("phase_mask", TargetedDatabase.WrathOfTheLichKing, TargetedDatabase.Cataclysm)]
         public uint? PhaseMask;
 
         [DBFieldName("phase_id", TargetedDatabase.Cataclysm)]
         public string PhaseID;
 
-        [DBFieldName("phase_group")]
+        [DBFieldName("phase_group", TargetedDatabase.Cataclysm)]
         public uint? PhaseGroup;
 
         [DBFieldName("position_x")]