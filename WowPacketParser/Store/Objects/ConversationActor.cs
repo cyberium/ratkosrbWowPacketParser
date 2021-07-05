TrinityCore/WowPacketParser

Update Column for conversation_actors from CreatureModelId to Creatur… (#602)

WowPacketParser/Store/Objects/ConversationActor.cs
@@ -19,8 +19,8 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("CreatureId", TargetedDatabase.Shadowlands)]
         public uint? CreatureId;
 
-        [DBFieldName("CreatureModelId", TargetedDatabase.Shadowlands)]
-        public uint? CreatureModelId;
+        [DBFieldName("CreatureDisplayInfoId", TargetedDatabase.Shadowlands)]
+        public uint? CreatureDisplayInfoId;
 
         public WowGuid Guid;
 

cyberium/ratkosrbWowPacketParser

WowPacketParser/Store/Objects/ConversationActor.cs
@@ -19,8 +19,8 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("CreatureId", TargetedDbExpansion.Shadowlands)]
         public uint? CreatureId;
 
-        [DBFieldName("CreatureModelId", TargetedDbExpansion.Shadowlands)]
-        public uint? CreatureModelId;
+        [DBFieldName("CreatureDisplayInfoId", TargetedDbExpansion.Shadowlands)]
+        public uint? CreatureDisplayInfoId;
 
         public WowGuid Guid;
 