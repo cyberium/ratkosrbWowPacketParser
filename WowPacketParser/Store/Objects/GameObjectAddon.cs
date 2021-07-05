TrinityCore/WowPacketParser

Output WorldEffectID and AIAnimKitID for gameobject_addon(_template) data

WowPacketParser/Store/Objects/GameObjectAddon.cs
@@ -1,3 +1,4 @@
+using WowPacketParser.Enums;
 using WowPacketParser.SQL;
 
 namespace WowPacketParser.Store.Objects
@@ -19,5 +20,11 @@ namespace WowPacketParser.Store.Objects
 
         [DBFieldName("parent_rotation3")]
         public float? parentRot3;
+
+        [DBFieldName("WorldEffectID", TargetedDatabase.Legion)]
+        public uint? WorldEffectID;
+
+        [DBFieldName("AIAnimKitID", TargetedDatabase.Shadowlands)]
+        public uint? AIAnimKitID;
     }
 }

WowPacketParser/Store/Objects/GameObjectTemplateAddon.cs
@@ -14,5 +14,11 @@ namespace WowPacketParser.Store.Objects
 
         [DBFieldName("flags")]
         public GameObjectFlag? Flags;
+
+        [DBFieldName("WorldEffectID", TargetedDatabase.Legion)]
+        public uint? WorldEffectID;
+
+        [DBFieldName("AIAnimKitID", TargetedDatabase.Shadowlands)]
+        public uint? AIAnimKitID;
     }
 }

cyberium/ratkosrbWowPacketParser

WowPacketParser/Store/Objects/GameObjectAddon.cs
@@ -1,3 +1,4 @@
+using WowPacketParser.Enums;
 using WowPacketParser.SQL;
 
 namespace WowPacketParser.Store.Objects
@@ -19,5 +20,11 @@ namespace WowPacketParser.Store.Objects
 
         [DBFieldName("parent_rotation3")]
         public float? parentRot3;
+
+        [DBFieldName("WorldEffectID", TargetedDbExpansion.Legion)]
+        public uint? WorldEffectID;
+
+        [DBFieldName("AIAnimKitID", TargetedDbExpansion.Shadowlands)]
+        public uint? AIAnimKitID;
     }
 }

WowPacketParser/Store/Objects/GameObjectTemplateAddon.cs
@@ -14,5 +14,11 @@ namespace WowPacketParser.Store.Objects
 
         [DBFieldName("flags")]
         public GameObjectFlag? Flags;
+
+        [DBFieldName("WorldEffectID", TargetedDbExpansion.Legion)]
+        public uint? WorldEffectID;
+
+        [DBFieldName("AIAnimKitID", TargetedDbExpansion.Shadowlands)]
+        public uint? AIAnimKitID;
     }
 }