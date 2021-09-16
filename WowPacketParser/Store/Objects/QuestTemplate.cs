TrinityCore/WowPacketParser

Update some 9.1.0 packets

WowPacketParser/Store/Objects/QuestTemplate.cs
@@ -179,6 +179,9 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("PortraitGiverMount", TargetedDatabase.BattleForAzeroth)]
         public uint? PortraitGiverMount;
 
+        [DBFieldName("PortraitGiverModelSceneID", TargetedDatabase.Shadowlands)]
+        public int? PortraitGiverModelSceneID;
+
         [DBFieldName("QuestTurnInPortrait", TargetedDatabase.Cataclysm, TargetedDatabase.WarlordsOfDraenor)]
         [DBFieldName("PortraitTurnIn", TargetedDatabase.WarlordsOfDraenor)]
         public uint? QuestTurnInPortrait;

WowPacketParserModule.V9_0_1_36216/Parsers/CreatureHandler.cs
@@ -105,6 +105,8 @@ namespace WowPacketParserModule.V9_0_1_36216.Parsers
             response.Expansion = (uint?) creature.RequiredExpansion ?? 0;
             creature.VignetteID = (uint)packet.ReadInt32("VignetteID");
             creature.UnitClass = (uint)packet.ReadInt32E<Class>("UnitClass");
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V9_1_0_39185))
+                creature.CreatureDifficultyID = packet.ReadInt32("CreatureDifficultyID");
             creature.WidgetSetID = packet.ReadInt32("WidgetSetID");
             creature.WidgetSetUnitConditionID = packet.ReadInt32("WidgetSetUnitConditionID");
 

cyberium/ratkosrbWowPacketParser

WowPacketParser/Store/Objects/QuestTemplate.cs
@@ -197,6 +197,9 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("PortraitGiverMount", TargetedDbExpansion.BattleForAzeroth)]
         public uint? PortraitGiverMount;
 
+        [DBFieldName("PortraitGiverModelSceneID", TargetedDbExpansion.Shadowlands)]
+        public int? PortraitGiverModelSceneID;
+
         [DBFieldName("PortraitTurnIn", TargetedDbExpansion.BurningCrusadeClassic, TargetedDbExpansion.Zero)]
         [DBFieldName("QuestTurnInPortrait", TargetedDbExpansion.Cataclysm, TargetedDbExpansion.WarlordsOfDraenor)]
         [DBFieldName("PortraitTurnIn", TargetedDbExpansion.WarlordsOfDraenor)]

WowPacketParserModule.V9_0_1_36216/Parsers/CreatureHandler.cs
@@ -91,6 +91,8 @@ namespace WowPacketParserModule.V9_0_1_36216.Parsers
             creature.RequiredExpansion = packet.ReadInt32E<ClientType>("RequiredExpansion");
             creature.VignetteID = (uint)packet.ReadInt32("VignetteID");
             creature.UnitClass = (uint)packet.ReadInt32E<Class>("UnitClass");
+            if (ClientVersion.AddedInVersion(ClientVersionBuild.V9_1_0_39185))
+                creature.DifficultyID = packet.ReadInt32("CreatureDifficultyID");
             creature.WidgetSetID = packet.ReadInt32("WidgetSetID");
             creature.WidgetSetUnitConditionID = packet.ReadInt32("WidgetSetUnitConditionID");
 