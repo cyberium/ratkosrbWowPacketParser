ba5065512fcefda5de5750e5846c38fdb93a9b1c
ratkosrb <ratkomladic2@abv.bg>
2022-01-18 15:04:51 +0200

    Include placeholder for sniff_id_list in INSERT statements.

--- WowPacketParser/SQL/IDataModel.cs
+++ WowPacketParser/SQL/IDataModel.cs
@@ -13,5 +13,8 @@ namespace WowPacketParser.SQL
     {
         public int SniffId = 0;
         public SortedSet<int> SniffIdList = null;
+
+        [DBFieldName("sniff_id_list")]
+        public readonly string SniffIdListPH = ""; // Placeholder to include an empty string in insert
     }
 }
