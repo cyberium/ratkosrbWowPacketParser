8e489391c3b92df41bc5520caca26cfa1f257c41
ratkosrb <ratkomladic2@abv.bg>
2022-03-10 19:45:24 +0200

    Add SMSG_PVP_MATCH_STATISTICS structure by modox.

--- WowPacketParserModule.V2_5_1_38707/Parsers/BattlegroundHandler.cs
+++ WowPacketParserModule.V2_5_1_38707/Parsers/BattlegroundHandler.cs
@@ -27,5 +27,87 @@ namespace WowPacketParserModule.V2_5_1_38707.Parsers
 
             packet.ReadByte("TeamSize");
         }
+
+        public static void ReadPvPMatchPlayerStatistics(Packet packet, params object[] idx)
+        {
+            packet.ReadPackedGuid128("PlayerGUID", idx);
+            packet.ReadUInt32("Kills", idx);
+            packet.ReadUInt32("DamageDone", idx);
+            packet.ReadUInt32("HealingDone", idx);
+            var statsCount = packet.ReadUInt32("StatsCount", idx);
+            packet.ReadInt32("PrimaryTalentTree", idx);
+            packet.ReadInt32E<Gender>("Sex", idx);
+            packet.ReadInt32E<Race>("Race", idx);
+            packet.ReadInt32E<Class>("Class", idx);
+            packet.ReadInt32("CreatureID", idx);
+            packet.ReadInt32("HonorLevel", idx);
+            packet.ReadInt32("Role", idx); // @TODO only confirmed for 2.5.3
+
+            for (int i = 0; i < statsCount; i++)
+                packet.ReadUInt32("Stats", i, idx);
+
+            packet.ResetBitReader();
+
+            packet.ReadBit("Faction", idx);
+            packet.ReadBit("IsInWorld", idx);
+
+            var hasHonor = packet.ReadBit("HasHonor", idx);
+            var hasPreMatchRating = packet.ReadBit("HasPreMatchRating", idx);
+            var hasRatingChange = packet.ReadBit("HasRatingChange", idx);
+            var hasPreMatchMMR = packet.ReadBit("HasPreMatchMMR", idx);
+            var hasMmrChange = packet.ReadBit("HasMmrChange", idx);
+
+            packet.ResetBitReader();
+
+            if (hasHonor)
+                V6_0_2_19033.Parsers.BattlegroundHandler.ReadHonorData(packet, "Honor");
+
+            if (hasPreMatchRating)
+                packet.ReadUInt32("PreMatchRating", idx);
+
+            if (hasRatingChange)
+                packet.ReadInt32("RatingChange", idx);
+
+            if (hasPreMatchMMR)
+                packet.ReadUInt32("PreMatchMMR", idx);
+
+            if (hasMmrChange)
+                packet.ReadInt32("MmrChange", idx);
+        }
+
+        [Parser(Opcode.SMSG_PVP_LOG_DATA)]
+        [Parser(Opcode.SMSG_PVP_MATCH_STATISTICS)]
+        public static void HandlePvPLogData(Packet packet)
+        {
+            var hasRatings = packet.ReadBit("HasRatings");
+            var hasUnk = packet.ReadBit("HasUnk");
+            var hasWinner = packet.ReadBit("HasWinner");
+
+            if (hasUnk)
+            {
+                var strLens = new uint[2];
+                for (int i = 0; i < 2; i++)
+                    strLens[i] = packet.ReadBits("UnkBits", 7);
+
+                for (int i = 0; i < 2; i++)
+                {
+                    packet.ReadPackedGuid128("UnkGuid128");
+                    packet.ReadWoWString((int)strLens[i]);
+                }
+            }
+
+            var statisticsCount = packet.ReadUInt32();
+            for (int i = 0; i < 2; i++)
+                packet.ReadSByte("PlayerCount", i);
+
+            if (hasRatings)
+                V8_0_1_27101.Parsers.BattlegroundHandler.ReadRatingData820(packet, "Ratings");
+
+            if (hasWinner)
+                packet.ReadByte("Winner");
+
+            for (int i = 0; i < statisticsCount; i++)
+                ReadPvPMatchPlayerStatistics(packet, "Statistics", i);
+        }
     }
 }
