376a6f82da6cc27a9162cf1d5d870011348df0af
ratkosrb <ratkomladic2@abv.bg>
2022-03-11 02:03:58 +0200

    Add SMSG_AREA_TRIGGER_MESSAGE.

---
+++ WowPacketParserModule.V1_13_2_31446/Parsers/AreaTriggerHandler.cs
@@ -0,0 +1,15 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V1_13_2_31446.Parsers
+{
+    public static class AreaTriggerHandler
+    {
+        [Parser(Opcode.SMSG_AREA_TRIGGER_MESSAGE)]
+        public static void HandleAreaTriggerReShape(Packet packet)
+        {
+            packet.ReadUInt32("AreaTriggerID");
+        }
+    }
+}
