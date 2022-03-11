TrinityCore/WowPacketParser

9.2.0 packet structures (excluding SMSG_UPDATE_OBJECT)

WowPacketParser/Store/Objects/AreaTriggerCreateProperties.cs
@@ -48,8 +48,8 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("Shape", TargetedDatabase.Shadowlands)]
         public byte? Shape;
 
-        [DBFieldName("ShapeData", TargetedDatabase.Shadowlands, 6, true)]
-        public float?[] ShapeData = { 0, 0, 0, 0, 0, 0 };
+        [DBFieldName("ShapeData", TargetedDatabase.Shadowlands, 8, true)]
+        public float?[] ShapeData = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
         [DBFieldName("VerifiedBuild")]
         public int? VerifiedBuild = ClientVersion.BuildInt;

WowPacketParser/Store/Objects/PlayerChoice.cs
@@ -16,8 +16,17 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("SoundKitId", TargetedDatabase.BattleForAzeroth)]
         public uint? SoundKitId;
 
+        [DBFieldName("CloseSoundKitId", TargetedDatabase.Shadowlands)]
+        public uint? CloseSoundKitId;
+
+        [DBFieldName("Duration", TargetedDatabase.Shadowlands)]
+        public long? Duration;
+
         [DBFieldName("Question")]
         public string Question;
+
+        [DBFieldName("PendingChoiceText", TargetedDatabase.Shadowlands)]
+        public string PendingChoiceText;
 
         [DBFieldName("HideWarboardHeader", TargetedDatabase.Legion)]
         public int HideWarboardHeader;

cyberium/ratkosrbWowPacketParser

WowPacketParser/Store/Objects/AreaTriggerCreateProperties.cs
@@ -47,8 +47,8 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("Shape", TargetedDbExpansion.Shadowlands)]
         public byte? Shape;
 
-        [DBFieldName("ShapeData", TargetedDbExpansion.Shadowlands, 6, true)]
-        public float?[] ShapeData = { 0, 0, 0, 0, 0, 0 };
+        [DBFieldName("ShapeData", TargetedDbExpansion.Shadowlands, 8, true)]
+        public float?[] ShapeData = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
         [DBFieldName("VerifiedBuild")]
         public int? VerifiedBuild = ClientVersion.BuildInt;

WowPacketParser/Store/Objects/PlayerChoice.cs
@@ -16,8 +16,17 @@ namespace WowPacketParser.Store.Objects
         [DBFieldName("SoundKitId", TargetedDbExpansion.BattleForAzeroth)]
         public uint? SoundKitId;
 
+        [DBFieldName("CloseSoundKitId", TargetedDbExpansion.Shadowlands)]
+        public uint? CloseSoundKitId;
+
+        [DBFieldName("Duration", TargetedDbExpansion.Shadowlands)]
+        public long? Duration;
+
         [DBFieldName("Question")]
         public string Question;
+
+        [DBFieldName("PendingChoiceText", TargetedDbExpansion.Shadowlands)]
+        public string PendingChoiceText;
 
         [DBFieldName("HideWarboardHeader", TargetedDbExpansion.Legion)]
         public int HideWarboardHeader;